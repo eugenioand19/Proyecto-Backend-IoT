e279cfd5d79c92f17a76609668432c783ab3c8b4
Eugenioand <eugenioand18@gmail.com>
2024-10-03 12:38:01 -0500

    cambio en la configuracion del jwt

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 from decouple import config
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = config('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = config('JWT_SECRET_KEY', 'super_secret_key_change_this_in_production')
-    JWT_ACCESS_TOKEN_EXPIRES = config('JWT_ACCESS_TOKEN_EXPIRES',3600 )
-    JWT_REFRESH_TOKEN_EXPIRES = config('JWT_REFRESH_TOKEN_EXPIRES', 86400)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=int(config('JWT_ACCESS_TOKEN_EXPIRES', 3600)))
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(seconds=int(config('JWT_REFRESH_TOKEN_EXPIRES', 86400)))
 
 
 class DevelopmentConfig(Config):
