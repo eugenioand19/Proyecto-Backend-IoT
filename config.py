2f64e0d832a41154c735f1f5a4b76bab87cbc0af
Eugenioand <eugenioand18@gmail.com>
2024-11-06 13:01:51 -0500

    Cambios en la configuracion

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class DevelopmentConfig(Config):
     DB_PORT = config('DB_PORT')
     DB_NAME = config('DB_NAME')
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = config('DATA_BASE_URL_DEV',f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}") 
 
 class ProductionConfig(Config):
     DEBUG = True
