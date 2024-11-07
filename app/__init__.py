302ff9b04f7d90f8f9a23e3cd51dd84731bf9ecf
Fabian Lozano <fabianlozano044@gmail.com>
2024-11-06 23:43:29 -0500

    feat: Update CORS configuration for different environments and add venv to .gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,22 @@ from db import db
 from flask_marshmallow import Marshmallow
 from app.utils.error.error_handlers import register_error_handlers, setup_jwt_handlers
 from app.routes.central_routes import register_blueprints
-from flask import Flask
-from db import db
-from flask_marshmallow import Marshmallow
 from app.routes.user_route import user_bp
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger
+from flask_cors import CORS
+import os
 
 
 
 app = Flask(__name__)
+
+env = os.environ.get('FLASK_ENV', 'development')
+
+if env == 'development':
+    CORS(app, resources={r"/*": {"origins": "https://proyecto-front-end-iot.vercel.app/"}})
+elif env == 'production':
+    CORS(app, resources={r"/*": {"origins": "https://wetlandmanager.vercel.app/"}})
 
 def init_app(config):
    
