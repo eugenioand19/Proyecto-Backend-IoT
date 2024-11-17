870b1617636e2f3ed6308ec667bcdd0c18ea5999
Fabian Lozano <fabianlozano044@gmail.com>
2024-11-17 16:14:03 -0500

    feat: update sensor query schema to unify sorting format and enhance type sensor validation

--- app/schemas/sensor_schema.py
+++ app/schemas/sensor_schema.py
@@ -3,6 +3,8 @@ from app.models.sensor import Sensor
 from marshmallow import Schema, ValidationError, fields, validate
 from app.models.type_sensor import TypeSensor
 from app.utils.delimited_list import DelimitedListField
+import re
+
 class SensorSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Sensor
@@ -14,32 +16,16 @@ class SensorQuerySchema(Schema):
     page_size = fields.Int(required=True, description="Page size", validate=validate.Range(min=1))
     page = fields.Int(required=True, description="Page number", validate=validate.Range(min=1))
     text_search = fields.Str(required=False, description="Search query")
-    sort_property = fields.Str(required=False, description="Sort property", 
-                               validate=validate.OneOf(["created_at", "name"]))
-    sort_order = fields.Str(required=False, description="Sort order", 
-                            validate=validate.OneOf(["ASC", "DESC"]))
+    sort = fields.Str(required=False, description="Sort in the format 'property.order'", validate=validate.Regexp(r'^[\w-]+\.(asc|desc)$', flags=re.IGNORECASE))
     
     valid_statuses = ["ACTIVE","INACTIVE"]
 
     # Passing the list of allowed values for validation
     statusList = DelimitedListField(
         allowed_values=valid_statuses,
-        required=False,
-        description="Comma-separated list of status values."
+        description="Comma-separated list of statuses"
     )
-    valid_types = ["PH","OD","TEMP","TURB","CAUD_EN","CAUD_SAL"]
-
-    # Passing the list of allowed values for validation
     typesList = DelimitedListField(
-        allowed_values=valid_types,
-        required=False,
-        description="Comma-separated list of status values."
+        allowed_values=[type_sensor.name for type_sensor in TypeSensor.query.all()],
+        description="Comma-separated list of type sensors"
     )
-
-class TypeSensorSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = TypeSensor
-        load_instance = True
-
-class SensorQuerySelectSchema(Schema):
-    text_search = fields.Str(required=False, description="Search query")
